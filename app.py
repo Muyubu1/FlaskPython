4c2a4b03719792d7f0c6254e7d19031e82cc6d14
Muyubu1 <muhammet226179@ogr.duzce.edu.tr>
2024-10-27 16:08:49 +0300

    Add name input form with WTForms, Bootstrap, and dynamic greeting support

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
+from forms.forms import NamerForm #formu entegre ettik
+
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'my_super_secret_key'
 @app.route("/")
 def home():
     stuff="<h1>Hello, World!</h1>"
@@ -29,7 +31,16 @@ def page_not_found(error):
 def internal_server_error(error):
     return render_template("500.html"), 500
 
+@app.route("/name", methods=["GET", "POST"])
+def name():
+    form = NamerForm()
+    name = None
 
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''  # Formu temizle
+
+    return render_template("index.html", form=form, name=name)
 
 
 if __name__ == "__main__":
